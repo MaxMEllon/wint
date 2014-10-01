fe57fa44980ff058eb71e3384be76171a21b8054
gembaf <s11t230@stmail.eng.kagawa-u.ac.jp>
2014-10-01 17:34:35 +0900

    ログアウト機能を実装する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  before_action :current_user
+  before_action :current_player
 
   def current_user
     @current_user = User.where(id: session[:uid]).first if session[:uid]
