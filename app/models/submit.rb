f23f55455656b514dc8aca7306661d46b0b4099a
gembaf <s11t230@stmail.eng.kagawa-u.ac.jp>
2014-10-16 13:46:46 +0900

    提出ファイルのステータスによってマイページの行の色を変える

--- app/models/submit.rb
+++ app/models/submit.rb
@@ -4,14 +4,14 @@ class Submit < ActiveRecord::Base
 
   validates_presence_of :data_dir
 
-  STATUS_RUNNINNG = 0
+  STATUS_RUNNING = 0
   STATUS_SUCCESS = 1
   STATUS_COMPILE_ERROR = 2
   STATUS_EXEC_ERROR = 3
 
   def self.status_options
     {
-      STATUS_RUNNINNG => '実行中',
+      STATUS_RUNNING => '実行中',
       STATUS_SUCCESS => '成功',
       STATUS_COMPILE_ERROR => 'コンパイルエラー',
       STATUS_EXEC_ERROR => '実行時エラー'
