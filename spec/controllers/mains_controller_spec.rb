3512406e571293ed68735869409771e433dd59b4
gembaf <gembaf.github@gmail.com>
2015-08-30 07:05:57 +0000

    [add] refs #2 ログイン成功/失敗のテストを追加

--- spec/controllers/mains_controller_spec.rb
+++ spec/controllers/mains_controller_spec.rb
@@ -1,5 +1,20 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe MainsController, :type => :controller do
+RSpec.describe MainsController, type: :controller do
+  describe 'ログイン' do
+    scenario '失敗する' do
+      visit login_path
+      click_on 'ログイン'
+      expect(page).to have_content 'ログイン失敗'
+    end
 
+    scenario '成功する' do
+      visit login_path
+      fill_in 'user[snum]', with: 's11t230'
+      fill_in 'user[password]', with: 'hoge'
+      click_on 'ログイン'
+      expect(page).to have_content '分析ページ'
+    end
+  end
 end
+
