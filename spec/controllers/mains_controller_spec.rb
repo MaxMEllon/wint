f2e093eacd445b5b4915c2d0364511b5f719b4f0
gembaf <gembaf.github@gmail.com>
2015-09-02 05:44:37 +0000

    [clean] refs #2 重複していたコードを削除

--- spec/controllers/mains_controller_spec.rb
+++ spec/controllers/mains_controller_spec.rb
@@ -1,32 +1,5 @@
 require 'spec_helper'
 
 RSpec.feature MainsController, type: :feature do
-  background do
-    visit login_path
-  end
-
-  feature '失敗する' do
-    click_on 'ログイン'
-    expect(page).to have_content 'ログイン失敗'
-  end
-
-  feature '成功する' do
-    scenario '管理者の場合' do
-      given(:user) { create :admin }
-      fill_in 'user[snum]', with: user.snum
-      fill_in 'user[password]', with: user.password
-      click_on 'ログイン'
-      expect(page).to have_content '分析ページ'
-    end
-  end
-
-  #  it '学生の場合' do
-  #    given(:user) { create :student }
-  #    fill_in 'user[snum]', with: user.snum
-  #    fill_in 'user[password]', with: user.password
-  #    click_on 'ログイン'
-  #    expect(page).to have_content 'hoge'
-  #  end
-  #end
 end
 
