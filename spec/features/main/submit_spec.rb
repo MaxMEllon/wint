9eaeecdb8218e2a7fd295e0fcea96873d5f416c9
gembaf <gembaf.github@gmail.com>
2015-10-03 08:38:00 +0000

    Add tests of each submission files

--- spec/features/main/submit_spec.rb
+++ spec/features/main/submit_spec.rb
@@ -1,7 +1,18 @@
 feature '戦略の提出', js: true, state: :main_mypage do
+  def submit(filename)
+    path = "#{Rails.root}/spec/factories/data/001/source/0001/001/"
+    fill_in 'submit[data_dir]', with: File.read(path + filename)
+    click_button '提出'
+    wait_for_action
+  end
+
   background do
     click_button '戦略提出'
     wait_for_action
+  end
+
+  context '何も入力していない場合' do
+    pending
   end
 
   # context '何も入力していない場合' do
@@ -15,22 +26,40 @@ feature '戦略の提出', js: true, state: :main_mypage do
   #   end
   # end
 
-  context '全て入力している場合' do
-    background do
-      path = "#{Rails.root}/spec/factories/data/001/source/0001/001/"
-      fill_in 'submit[data_dir]', with: File.read(path + 'PokerOpe.c')
-      fill_in 'submit[comment]', with: 'hoge狙い'
-      click_button '提出'
-      wait_for_action
-    end
+  context '正しい戦略を提出した場合' do
+    background { submit 'success.c' }
 
-    scenario '提出履歴に表示が追加される' do
-      expect(page).to have_content 'hoge狙い'
-    end
-
-    scenario 'ステータスが成功になる' do
+    scenario '成功と表示される' do
       expect(page).to have_content '成功'
     end
+  end
+
+  context 'コンパイルエラーとなる戦略を提出した場合' do
+    background { submit 'compile_error.c' }
+
+    scenario 'コンパイルエラーと表示される' do
+      expect(page).to have_content 'コンパイルエラー'
+    end
+  end
+
+  context '実行時エラーとなる戦略を提出した場合' do
+    background { submit 'execute_error.c' }
+
+    scenario '実行時エラーと表示される' do
+      expect(page).to have_content '実行時エラー'
+    end
+  end
+
+  context 'シンタックスエラーとなる戦略を提出した場合' do
+    background { submit 'syntax_error.c' }
+
+    scenario '危険なコードと表示される' do
+      expect(page).to have_content '危険なコード'
+    end
+  end
+
+  context '時間超過となる戦略を提出した場合' do
+    pending
   end
 end
 
